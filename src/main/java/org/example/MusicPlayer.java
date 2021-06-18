402fdd3c8e8bfc1c67583513c2c9d3c86a8a3914
Umid Karimov <karimovumid95@gmail.com>
2021-06-18 07:19:14 +0400

    changes

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,13 +1,56 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private byte volume;
+    private List<Music> musicList = new ArrayList<>();
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void printMusicList() {
+        for (Music list:this.musicList) {
+            System.out.println(list.getSong());
+        }
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public MusicPlayer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getVolume() {
+        return volume;
+    }
+
+    public void setVolume(byte volume) {
+        this.volume = volume;
+    }
+
     public void playMusic() {
         System.out.println("Playing " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,11 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
         //Music beanMusic = context.getBean("musicBean",Music.class);
         //MusicPlayer player = new MusicPlayer(beanMusic);
         MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
         player.playMusic();
+        System.out.println(player.getVolume());
+        System.out.println(player.getName());
+        player.printMusicList();
         context.close();
 
     }
